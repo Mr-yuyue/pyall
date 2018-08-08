b6d0506359b58e5e6e5aa32ee68290536a78c349
li qq <lqq2256270@qq.com>
2018-08-08 18:02:09 +0800

    20180808更新
    ha

--- Sample/Internet/sRequests.py
+++ Sample/Internet/sRequests.py
@@ -1,5 +1,18 @@
 import requests
 
-r = requests.post('https://www.zhihu.com/')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+
+postData = {}
+sandCookies = {'li': '123'}
+r = requests.post('https://www.zhihu.com/', data=postData, headers=headers, cookies=sandCookies)
+
+# print(r.content)
+# print(r.encoding)
 # print(r.text)
-print(r.content)
+# print(r.headers)
+print(r.status_code)
+
+for cookie in r.cookies.items():
+    # print(r.cookies.get(cookie))
+    print(cookie)
