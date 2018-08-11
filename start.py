b51b4b4e62dc08239041422f6ee7c4778b7e28f2
li qq <lqq2256270@qq.com>
2018-08-11 10:13:58 +0800

    mac20180811
    1111

--- start.py
+++ start.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+# try:
+#     f = open('测试文本读取', 'r')
+#     print(f.read())
+# finally:
+#     if f:
+#         f.close()
+
+with open('测试文本读取', 'r', encoding='GBK', errors="ignore") as f:
+    print(f.read())
